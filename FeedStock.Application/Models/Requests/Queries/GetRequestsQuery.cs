90682a8dd748c9c4fbfc5d1f3f3133b09f045b4c
stryder123451 <delphiprogrammer1@gmail.com>
2022-08-01 23:22:48 +0300

    queries

---
+++ FeedStock.Application/Models/Requests/Queries/GetRequestsQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedStock.Application.Models.Requests.Queries
+{
+    public class GetRequestsQuery: IRequest<EasyOrders>
+    {
+
+    }
+}
