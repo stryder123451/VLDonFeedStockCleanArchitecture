e64378aa663b94b0b18a6891b8e3aab17a7b7f60
stryder123451 <delphiprogrammer1@gmail.com>
2022-08-03 11:11:23 +0300

    DI

---
+++ FeedStock.Application/Dependency/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedStock.Application.Dependency
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ FeedStock.Persistence/Dependency/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using FeedStock.Application.Interfaces;
+using FeedStock.Persistence.DbContextClass;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedStock.Persistence.Dependency
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<FeedStockDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            }
+            );
+            services.AddScoped<IFeedStockDbContext>(provider => provider.GetService<FeedStockDbContext>());
+            return services;
+        }
+    }
+}
